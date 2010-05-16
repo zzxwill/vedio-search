db1e781e11f32c162d8e119b541e0430b0d30e94
zzxwill <zzxwill@c68eb2b8-cecc-f7d2-e3ce-51afc94c7d57>
2010-05-16 18:06:01 +0000

    加入了TF-IDF模块

--- ictclas4j/src/tfidf/Item.java
+++ ictclas4j/src/tfidf/Item.java
@@ -5,50 +5,63 @@ package tfidf;
 
 /**
  * @author Administrator
- *
+ * 
  */
 public class Item {
-	int subjectNo;
-	int actionNo;
-	int sceneNo;
-	int bodyPartNo;
+	public int subjectNo = 0;
+	public int actionNo = 0;
+	public int sceneNo = 0;
+	public int bodyPartNo = 0;
 	/*
 	 * 一个句子中，这四个成分的总个数
 	 */
-	int sub_act_sce_bod_No;
+	public int sub_act_sce_bod_No = 0;
+	
+	/*
+	 * 总的句子数
+	 */
+	public int allSentencesNo = 0;
 	
 	
+
 	public int getSubjectNo() {
 		return subjectNo;
 	}
+
 	public void setSubjectNo(int subjectNo) {
 		this.subjectNo = subjectNo;
 	}
+
 	public int getActionNo() {
 		return actionNo;
 	}
+
 	public void setActionNo(int actionNo) {
 		this.actionNo = actionNo;
 	}
+
 	public int getSceneNo() {
 		return sceneNo;
 	}
+
 	public void setSceneNo(int sceneNo) {
 		this.sceneNo = sceneNo;
 	}
+
 	public int getBodyPartNo() {
 		return bodyPartNo;
 	}
+
 	public void setBodyPartNo(int bodyPartNo) {
 		this.bodyPartNo = bodyPartNo;
 	}
+
 	public int getSub_act_sce_bod_No() {
 		return sub_act_sce_bod_No;
 	}
+
 	public void setSub_act_sce_bod_No(int sub_act_sce_bod_No) {
 		this.sub_act_sce_bod_No = sub_act_sce_bod_No;
 	}
-
-	
 
 }
